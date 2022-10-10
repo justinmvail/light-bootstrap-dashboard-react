dc903aee5e6f673dc07aae09232ff356b6a80edc
justinvail <justinmvail@gmail.com>
2022-10-10 18:01:25 -0400

    fixing background plugin to color radio buttons the correct color Refactoring some files to be jsx vs js for intellij color coding

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -21,7 +21,7 @@ import { useLocation, Route, Switch } from "react-router-dom";
 import AdminNavbar from "components/Navbars/AdminNavbar";
 import Footer from "components/Footer/Footer";
 import Sidebar from "components/Sidebar/Sidebar";
-import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
+import FixedPlugin from "components/FixedPlugin/FixedPlugin.jsx";
 
 import routes from "routes.js";
 
